48d9b51f8fefb78c216e3f988af929af9837539b
gorkemozucar <serkangorkemozucar@eskisehir.edu.tr>
2022-06-09 11:21:17 +0300

    Add project files.

---
+++ VerifyEmailForgotPasswordTutorial/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace VerifyEmailForgotPasswordTutorial.Data
+{
+    public class DbContext : DbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder
+                .UseSqlServer("Server=.\\SQLExpress;Database=userdb;Trusted_Connection=true;");
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
